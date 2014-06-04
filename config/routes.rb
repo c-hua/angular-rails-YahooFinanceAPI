5d5b66f72f24b8814a15f82308d0db0e6c13dcc6
Christie Hua <christiehua@gmail.com>
2014-06-03 21:23:18 -0700

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Stockwatch::Application.routes.draw do
     resources :stocks, defaults: {format: :json}
   end
   root 'watchlist#index'
-end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

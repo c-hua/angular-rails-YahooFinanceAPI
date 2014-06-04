965f8ef6a53fbc9c0818d1e31023591e890372e8
Christie Hua <christiehua@gmail.com>
2014-06-03 21:32:37 -0700

    AJAX for Yahoo Finance Data

--- app/assets/javascripts/angular/controllers/stocks_ctrl.js
+++ app/assets/javascripts/angular/controllers/stocks_ctrl.js
@@ -1,28 +1,76 @@
-app.controller('StocksCtrl', ['$scope', 'Stock', '$filter', '$http', function($scope, Stock, $filter, $http) {
+app.controller('StocksCtrl', ['$scope', 'Stock', '$filter', '$http', '$q', function($scope, Stock, $filter, $http, $q) {
   $scope.stocks = Stock.all();
   $scope.error = false;
 
-  $scope.createStock = function() {
-    var attr = {};
-    attr.symbol = $filter('uppercase')($scope.newSymbol);
-    $http({method: 'GET', url: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D" + "'" + attr.symbol + "'" + "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-}).success(function(data, status, headers, config) {
-     $scope.error = false;
-     attr.name = data.query.results.quote["Name"];
-     attr.bid = data.query.results.quote["Bid"];
-     attr.ask = data.query.results.quote["Ask"];
-     attr.year_low = data.query.results.quote["YearLow"];
-     attr.year_high = data.query.results.quote["YearHigh"];
-     var newStock = Stock.create(attr);
-     $scope.stocks.push(newStock);
-     $scope.newSymbol = "";
-}).error(function(data, status, headers, config) {
-     $scope.error = true;
-});
+
+  $scope.select2Options = {
+    'ajax': {
+      url: "/api/derivatives.json",
+      dataType: 'json',
+      data: function(term, page) {
+        return { q: term }; 
+      },
+      results: function(data, page) {
+        return { results: data };
+      }
+    }
   };
-  
+
+  $scope.$watch('newCompany', function() {
+    if ($scope.newCompany != "" && $scope.newCompany != null) {
+      $scope.createStock();
+    }
+  });
+
+  $scope.fetchYahooFinanceData = function(symbol) {
+    var deferred = $q.defer();
+    var stock = {};
+    $scope.loading = true;
+    $http({method: 'GET', url: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D" + "'" + symbol + "'" + "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"}).success(function(data, status, headers, config) {
+      stock.symbol = symbol;
+      stock.name = data.query.results.quote["Name"];
+      stock.bid = data.query.results.quote["Bid"];
+      stock.ask = data.query.results.quote["Ask"];
+      stock.year_low = data.query.results.quote["YearLow"];
+      stock.year_high = data.query.results.quote["YearHigh"];
+      $scope.loading = false;
+      deferred.resolve(stock);
+    }).error(function(data, status, headers, config) {
+      $scope.loading = false;
+      deferred.reject(status);
+    });
+    return deferred.promise;
+  };
+
+  $scope.requestOHLC = function (stockid) {
+    return Stock.ohlc(stockid);
+  }
+
+  $scope.createStock = function() {
+    $scope.fetchYahooFinanceData($filter('uppercase')($scope.newCompany["symbol"])).then(function(result) {
+      $scope.error = false;
+      $scope.stocks.push(Stock.create(result));
+      $scope.newCompany = '';
+    }, function(error) {
+      $scope.error = true;
+    });
+  };
+
   $scope.deleteStock = function(id, idx) {
     Stock.delete(id);
     $scope.stocks.splice(idx, 1);
   };
+
+  $scope.updateStock = function(id, idx) {
+    var stock = $scope.stocks[idx];
+    $scope.fetchYahooFinanceData(stock.symbol).then(function(result) {
+      $scope.error = false;
+      result.id = stock.id;
+      Stock.update(result);
+      $scope.stocks[idx] = result;
+    }, function(error) {
+      $scope.error = true;
+    });
+  };
+
 }]);

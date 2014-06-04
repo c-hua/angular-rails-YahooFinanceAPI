555faf3eba5253f2a9226f20b0fa9d8dc9d5dad7
Christie Hua <christiehua@gmail.com>
2014-06-03 21:15:22 -0700

    modified CSRF cookie

--- app/assets/javascripts/angular/controllers/stocks_ctrl.js
+++ app/assets/javascripts/angular/controllers/stocks_ctrl.js
@@ -1,7 +1,8 @@
 app.controller('StocksCtrl', ['$scope', 'Stock', function($scope, Stock) {
   $scope.stocks = Stock.all();
-
-    $scope.deleteStock = function(idx) {
+  
+  $scope.deleteStock = function(id, idx) {
     $scope.stocks.splice(idx, 1);
+    return Stock.delete(id);
   };
 }]);
